2fe8b0b6d969a6bbe1dfb3b6264e673527bd0dac
曹洋 <55371110+Rfddc@users.noreply.github.com>
2020-04-23 07:58:31 +0800

    提交

--- js/index.js
+++ js/index.js
@@ -4,14 +4,6 @@ $(function () {
     $(".vmall_ad").css("display", "none");
     $(".vmall_ad").hide();
   });
-  // bug*1   导航栏
-  $(".more_nb>ul").on("mouseover", function () {
-    $("#mor>a").css("color", "#ca151d");
-  });
-  $(".more_nb>ul").on("mouseout", function () {
-    $("#mor>a").css("color", "#afa27f");
-  });
-  // 右边的导航栏
- 
-  
+
+
 });
