f673fee8c0d8d31206938f2e5f88ff2ac82f31dc
曹洋 <55371110+Rfddc@users.noreply.github.com>
2020-05-05 08:09:21 +0800

    提交

---
+++ js/signin.js
@@ -0,0 +1,36 @@
+$(function () {
+    // $('.main_right_input').on('click', function () {
+    //     $(this).css({
+    //         "border": '1px solid #007dff'
+
+    //     })
+    // })
+    // $('.main_right_input').on("mouseout", function () {
+    //     $(this).css({
+    //         "border": '1px solid #f2f2f2'
+    //     })
+    // })
+
+    // $('.main_right_input').onblur = function () {
+    //     $(this).css({
+    //         "border": '1px solid #f2f2f2'
+    //     })
+    // }
+
+    $('#input').focus(function () {
+        alert("focus~!!")
+    
+        // $(this).css({
+        //     "border": '1px solid #007dff'
+
+        // })
+    })
+
+    // $('.main_right_input').blur(function () {
+    //     $(this).css({
+    //         "border": '1px solid #f2f2f2'
+
+    //     })
+    // })
+
+})
