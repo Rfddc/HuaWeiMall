c2aea287951feb00e5e155e66c4392ddd75571e5
曹洋 <55371110+Rfddc@users.noreply.github.com>
2020-05-06 20:24:38 +0800

    tijiao

--- js/signin.js
+++ js/signin.js
@@ -1,36 +1,42 @@
 $(function () {
-    // $('.main_right_input').on('click', function () {
-    //     $(this).css({
-    //         "border": '1px solid #007dff'
+  // $('.main_right_input').on('click', function () {
+  //     $(this).css({
+  //         "border": '1px solid #007dff'
 
-    //     })
-    // })
-    // $('.main_right_input').on("mouseout", function () {
-    //     $(this).css({
-    //         "border": '1px solid #f2f2f2'
-    //     })
-    // })
+  //     })
+  // })
+  // $('.main_right_input').on("mouseout", function () {
+  //     $(this).css({
+  //         "border": '1px solid #f2f2f2'
+  //     })
+  // })
 
-    // $('.main_right_input').onblur = function () {
-    //     $(this).css({
-    //         "border": '1px solid #f2f2f2'
-    //     })
-    // }
+  // $('.main_right_input').onblur = function () {
+  //     $(this).css({
+  //         "border": '1px solid #f2f2f2'
+  //     })
+  // }
 
-    $('#input').focus(function () {
-        alert("focus~!!")
-    
-        // $(this).css({
-        //     "border": '1px solid #007dff'
+  $(".main_right_input>input").focus(function () {
+    $(".main_right_input").css({
+      "border": "1px solid #4990ed",
+    });
+  });
 
-        // })
-    })
+  $(".main_right_input>input").blur(function () {
+    $(".main_right_input").css({
+      "border": "1px solid #f2f2f2",
+    });
+  });
+  $(".main_right_password>input").focus(function () {
+    $(".main_right_password").css({
+      "border": "1px solid #4990ed",
+    });
+  });
 
-    // $('.main_right_input').blur(function () {
-    //     $(this).css({
-    //         "border": '1px solid #f2f2f2'
-
-    //     })
-    // })
-
-})
+  $(".main_right_password>input").blur(function () {
+    $(".main_right_password").css({
+      "border": "1px solid #f2f2f2",
+    });
+  });
+});
